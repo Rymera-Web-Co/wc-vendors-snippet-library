d5f1fe51f6fa4c99b6b5cdded62d192890fb581f
Mrlunsuper <irisslee95@gmail.com>
2021-10-27 15:01:48 +0700

    Add CSS class to apply for vendor, terms and condition label in a registration

--- front/registration.php
+++ front/registration.php
@@ -12,3 +12,29 @@ function wcv_add_vendor_registration_note(){
 <p>This is a notice that you can display before the apply to be a vendor checkbox.</p>
 <?php
 }
+
+/**
+ * Add CSS class to apply for vendor label in a registration
+ *
+ * @param  string $class CSS classes.
+ * @return string
+ */
+function add_apply_vendor_label_css_class( $class ) {
+	$class .= 'example_css_class';
+
+	return $class;
+}
+add_filter( 'wcvendors_vendor_registration_apply_label_css_classes', 'add_apply_vendor_label_css_class', 10, 1 );
+
+/**
+ * Add CSS class to term and condition label in a registration
+ *
+ * @param  string $class CSS classes.
+ * @return string
+ */
+function add_term_label_css_class( $class ) {
+	$class .= 'example_css_class';
+
+	return $class;
+}
+add_filter( 'wcvendors_vendor_registration_term_label_css_classes', 'add_term_label_css_class', 10, 1 );
