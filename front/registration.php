adfa80f8c419b8d16a3953d75098ba42e33e59f2
Mrlunsuper <irisslee95@gmail.com>
2021-10-29 09:00:48 +0700

    Add wcv prefix to functions name

--- front/registration.php
+++ front/registration.php
@@ -19,12 +19,12 @@ function wcv_add_vendor_registration_note(){
  * @param  string $class CSS classes.
  * @return string
  */
-function add_apply_vendor_label_css_class( $class ) {
+function wcv_add_apply_vendor_label_css_class( $class ) {
 	$class .= 'example_css_class';
 
 	return $class;
 }
-add_filter( 'wcvendors_vendor_registration_apply_label_css_classes', 'add_apply_vendor_label_css_class', 10, 1 );
+add_filter( 'wcvendors_vendor_registration_apply_label_css_classes', 'wcv_add_apply_vendor_label_css_class', 10, 1 );
 
 /**
  * Add CSS class to term and condition label in a registration
@@ -32,9 +32,9 @@ add_filter( 'wcvendors_vendor_registration_apply_label_css_classes', 'add_apply_
  * @param  string $class CSS classes.
  * @return string
  */
-function add_term_label_css_class( $class ) {
+function wcv_add_term_label_css_class( $class ) {
 	$class .= 'example_css_class';
 
 	return $class;
 }
-add_filter( 'wcvendors_vendor_registration_term_label_css_classes', 'add_term_label_css_class', 10, 1 );
+add_filter( 'wcvendors_vendor_registration_term_label_css_classes', 'wcv_add_term_label_css_class', 10, 1 );
